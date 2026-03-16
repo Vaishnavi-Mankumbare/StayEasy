e3a39e86caf72d59fccb7b8dd2516a1da598a26b
Vaishnavi Mankumbare <mankumbrevaishnavi@gmail.com>
2026-03-16 15:31:28 +0530

    fix render port

--- server.js
+++ server.js
@@ -188,6 +188,8 @@ res.json({message:"Listing deleted"});
 
 /* Start server */
 
-app.listen(5000, ()=>{
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+  console.log("Server running on port " + PORT);
 });
